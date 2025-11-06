6c14fc4e843cb05e31e56557136902b5f9127057
Ariel <arielmedin577@gmail.com>
2025-11-06 11:25:51 -0300

    Agregando un nuevo modulo de cobros, y configurando la cantidad de hilos que se puede utilizar en la jvm

--- src/main/java/com/example/demo/modules/factura/listener/FacturaListener.java
+++ src/main/java/com/example/demo/modules/factura/listener/FacturaListener.java
@@ -2,7 +2,6 @@ package com.example.demo.modules.factura.listener;
 
 import com.example.demo.modules.factura.events.FacturaEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
